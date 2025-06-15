bdc0a172e61c6cd9ca589d1624dd880e2df855b3
Nipun <nipun22325@iiitd.ac.in>
2025-06-16 00:00:38 +0530

    minor changes using suggestions made by coderabbit

--- src/components/generated-avatar.tsx
+++ src/components/generated-avatar.tsx
@@ -3,6 +3,7 @@ import { botttsNeutral, initials } from "@dicebear/collection";
 
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useMemo } from "react";
 
 interface GeneratedAvatarProps {
   className?: string;
@@ -15,19 +16,27 @@ export const GeneratedAvatar = ({
   seed,
   variant,
 }: GeneratedAvatarProps) => {
-  let avatarSvg;
+  //   let avatarSvg;
 
-  if (variant === "botttsNeutral") {
-    avatarSvg = createAvatar(botttsNeutral, {
-      seed,
-    });
-  } else {
-    avatarSvg = createAvatar(initials, {
-      seed,
-      fontSize: 42,
-      fontWeight: 500,
-    });
-  }
+  //   if (variant === "botttsNeutral") {
+  //     avatarSvg = createAvatar(botttsNeutral, {
+  //       seed,
+  //     });
+  //   } else {
+  //     avatarSvg = createAvatar(initials, {
+  //       seed,
+  //       fontSize: 42,
+  //       fontWeight: 500,
+  //     });
+  //   }
+  const avatarSvg = useMemo(
+    () =>
+      variant === "botttsNeutral"
+        ? createAvatar(botttsNeutral, { seed })
+        : createAvatar(initials, { seed, fontSize: 42, fontWeight: 500 }),
+    [seed, variant]
+  );
+
   return (
     <Avatar className={cn(className)}>
       <AvatarImage src={avatarSvg.toDataUri()} alt="Avatar" />
