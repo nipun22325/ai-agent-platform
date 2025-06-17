1d216538b81f27249ec088e5736622ea335d2320
Nipun <nipun22325@iiitd.ac.in>
2025-06-17 23:28:22 +0530

    setup trpc + experiment with client & server component + explore prefetching

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,28 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client"; //import the auth client
-import { redirect } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  const { data: session } = authClient.useSession();
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Nipun" }));
 
-  if (!session) {
-    return <p> Loading... </p>;
-  }
-  return (
-    <div className="p-4 flex flex-col gap-y-4">
-      <p className="mt-2">You are logged in as {session.user.name} </p>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: { onSuccess: () => redirect("/sign-in") }, // Redirect to sign-in page after sign out
-          })
-        }
-        className="w-full"
-      >
-        Sign Out
-      </Button>
-    </div>
-  );
+  return <div className="p-4 flex flex-col gap-y-4">{data?.greeting}</div>;
 };
