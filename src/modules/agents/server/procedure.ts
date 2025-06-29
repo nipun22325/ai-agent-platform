83305cb0836f41fe2ca351035a0405a7ad91ea74
Nipun <nipun22325@iiitd.ac.in>
2025-06-29 14:36:05 +0530

    modified agents.getOne procedure to show agent only if user created it, otherwise throw TRPCError + added page for a single agent, selected through ID which can be accessed by clicking on a row of the DataTable on agents page

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -10,12 +10,12 @@ import {
   MAX_PAGE_SIZE,
   MIN_PAGE_SIZE,
 } from "@/constants";
-// import { TRPCError } from "@trpc/server";
+import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .query(async ({ input }) => {
+    .query(async ({ ctx, input }) => {
       const [existingAgent] = await db
         .select({
           //TODO: change to actual meeting count
@@ -23,8 +23,12 @@ export const agentsRouter = createTRPCRouter({
           ...getTableColumns(agents),
         })
         .from(agents)
-        .where(eq(agents.id, input.id));
-      // throw new TRPCError({ code: "BAD_REQUEST" });
+        .where(
+          and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id))
+        );
+      if (!existingAgent) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+      }
       return existingAgent;
     }),
   getMany: protectedProcedure
