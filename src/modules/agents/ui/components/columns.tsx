6b5bedf83ba6148485458630b3e81d7019dd2462
Nipun <nipun22325@iiitd.ac.in>
2025-06-29 13:41:20 +0530

    added ui for filters, namely search agents and pagination + added nuqs for handling URL search parameters, and syncing with filters + modified agents.getMany procedure to only show agents which the user has created and searched for and show them on pages depending on limit + synced RSC filters and client filters by modifying how we prefetch data in server component

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -38,7 +38,8 @@ export const columns: ColumnDef<AgentGetOne>[] = [
         className="flex items-center gap-x-2 [&>svg]:size-4"
       >
         <VideoIcon className="text-blue-700" />
-        {row.original.meetingCount ?? 0}
+        {row.original.meetingCount}{" "}
+        {row.original.meetingCount === 1 ? "Meeting" : "Meetings"}
       </Badge>
     ),
   },
